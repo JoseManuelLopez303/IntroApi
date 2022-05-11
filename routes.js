86e4ec1340caf72b8503ec2b4efd49a570d3f848
JoseManuelLopez303 <hermanoslopez539@gmail.com>
2022-05-11 11:47:03 -0600

    Mostrar usuarios

--- routes.js
+++ routes.js
@@ -5,7 +5,19 @@ const pool = require('../data/config');
 const router = app => {
     //Mostrar la mensaje de bienvenida de root
     app.get('/',(request,response)=> {
+        response.send({
         message: 'Bienvenido a Node.js Express Rest API'
     });
 
-}
+    });
+//Mostrar todos los usuario
+app.get('/users',(request, response) => {
+    pool.query('SELECT * FROM users',(error, result) => {
+        if(error) throw error;
+        response.send(result);
+    });
+});
+
+
+    }
+    
